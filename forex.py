a3b9f5ce1b3783e5c6a58cc1b8f3dd832d379a2a
josianator <josianator@gmail.com>
2016-06-22 15:23:28 -0400

    example 5

--- forex.py
+++ forex.py
@@ -3,20 +3,48 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
+import time
 
-
+date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack = True,
+                            delimiter = ',',
+                            converters = { 0 : mdates.strpdate2num('%Y%m%d%H%M%S')})
+                            
 def percentChange(startPoint, currentPoint):
-    return (100 * (currentPoint - startPoint) / startPoint)
+    return (100.00 * (float(currentPoint) - float(startPoint)) / float(startPoint))
+
+def patternFinder():
+    avgLine = ((bid + ask) / 2)
+    x = len(avgLine)
+    
+    y = 11
+    while y < x:
+        # I <3 magic numbers!
+        p1  = percentChange(avgLine[y - 10], avgLine[y - 9])
+        p2  = percentChange(avgLine[y - 10], avgLine[y - 8])
+        p3  = percentChange(avgLine[y - 10], avgLine[y - 7])
+        p4  = percentChange(avgLine[y - 10], avgLine[y - 6])
+        p5  = percentChange(avgLine[y - 10], avgLine[y - 5])
+        p6  = percentChange(avgLine[y - 10], avgLine[y - 4])
+        p7  = percentChange(avgLine[y - 10], avgLine[y - 3])
+        p8  = percentChange(avgLine[y - 10], avgLine[y - 2])
+        p9  = percentChange(avgLine[y - 10], avgLine[y - 1])
+        p10 = percentChange(avgLine[y - 10], avgLine[y - 0])
 
 
-
-
+        outcomeRange = avgLine[y + 20 : y + 30]
+        currentPoint = avgLine[y]
+        
+        print reduce(lambda x, y: x + y, outcomeRange) / len(outcomeRange)
+        print currentPoint
+        print ' ____'
+        print p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
+        
+        y += 1
+        
+        time.sleep(5555)
+        
 def graphRawFX():
 
-    date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack = True,
-                                delimiter = ',',
-                                converters = { 0 : mdates.strpdate2num('%Y%m%d%H%M%S')})
-                                
     fig = plt.figure(figsize = (10, 7))
     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan = 40, colspan = 40)
     
@@ -42,7 +70,9 @@ def graphRawFX():
     
     
 def main():
-    graphRawFX()
+    # graphRawFX()
+
+    patternFinder()
 
 
 if __name__ == '__main__':
